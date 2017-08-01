d14c68d87d7551245a84b28a40ecaea004def9b8
Dan Garland <dan@wegotcoders.com>
2017-08-01 14:55:24 +0100

    Allowing an edit option

--- src/components/userView.js
+++ src/components/userView.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 
 class UserView extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'show'
+    };
+
+    this.edit = this.edit.bind(this);
+  }
+
+  edit() {
+    this.setState( {
+      view: 'edit'
+    });
+  }
+
   render() {
+    var view;
+
+    if(this.state.view === 'show') {
+      view = <h2 onDoubleClick={this.edit}>{ this.props.user.fullName() }</h2>;
+    } else {
+      view = <div>
+          <input className='edit-field' value={ this.props.user.firstName } />
+          <input className='edit-field' value={ this.props.user.lastName } />
+        </div>;
+    }
+
     return <div className="user-view">
-      <h2>Dan Garland</h2>
+      { view }
     </div>;
   }
 
--- test/components/userView.js
+++ test/components/userView.js
@@ -3,23 +3,43 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import UserView from '../../src/components/userView';
+import User from '../../src/models/user';
 
 describe("userView component", function() {
   var component;
 
-  describe("render", function() {
-    beforeEach(function() {
-      component = shallow(<UserView />);
+  beforeEach(function() {
+    var user = new User({
+      firstName: 'Joe',
+      lastName: 'Perkins'
     });
 
+    component = shallow(<UserView user={ user } />);
+  });
+
+  describe("render", function() {
     it("creates a div with a class name of user-view", function() {
       expect(component.is("div")).to.eq(true);
       expect(component.hasClass("user-view")).to.eq(true);
     });
 
     it("displays the user's name in a heading", function() {
-      console.log(component.html()); // debug the react component
-      expect(component.contains(<h2>Dan Garland</h2>)).to.eq(true);
+      expect(component.find('h2').length).to.eq(1);
+      expect(component.find('h2').text()).to.eq('Joe Perkins');
+    });
+  });
+
+  describe("editing name", function() {
+    beforeEach(function() {
+      component.find("h2").last().simulate("dblclick");
+    });
+
+    it("displays the first and last names in edit boxes for the user to change", function() {
+      const inputs = component.find("input.edit-field");
+
+      expect(inputs).to.have.length(2);
+      expect(inputs.first().node.props.value).to.eq('Joe');
+      expect(inputs.last().node.props.value).to.eq('Perkins');
     });
   });
 
